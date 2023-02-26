3ba51cc4b0390fcc59fabf80bcf2d8f101dcebc0
Michiel Borkent <michielborkent@gmail.com>
2023-02-26 23:51:31 +0100

    enough for today

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Hi! Welcome to my CLJS showcase!
+# cljs-showcase
 
-Here starts the tutorial. We show that in ClojureScript you can add multiple numbers in one go:
+This repository showcases how you can showcase your ClojureScript library and
+make it interactive using [SCI](https://github.com/babashka/sci).
+
+To read the interactive version of this markdown file, go [here](https://borkdude.github.io/cljs-showcase).
+
+To add an interactive ClojureScript snippet, you have to surround your code
+blocks with a div that has a class `cljs-showcase`, like the one below. You can
+add your own styling, of course.
 
 <div style="width: 600px;" class="cljs-showcase">
 
@@ -24,6 +31,38 @@ Promise values are resolved and their results are shown:
 
 ``` clojure
 (-> (js/fetch "dude") (.then (fn [v] (.-status v))))
+```
+
+</div>
+
+To add your own libraries, go to [src/cljs_showcase/core.cljs](https://github.com/borkdude/cljs-showcase/blob/main/src/cljs_showcase/core.cljs)
+
+and use the SCI API to add your ClojureScript namespaces. E.g. to add [medley](https://github.com/weavejester/medley), we run:
+
+<div style="width: 600px;" class="cljs-showcase" data-cljs-showcase-no-editable="true" data-cljs-showcase-no-eval="true">
+
+``` clojure
+(def medley-ns (sci/copy-ns medley.core (sci/create-ns 'medley.core)))
+(ctx-store/swap-ctx! sci/merge-opts {:namespaces {'medley.core medley-ns}})
+```
+
+</div>
+
+After doing so, we can use medley interactively in a codeblock:
+
+<div style="width: 600px;" class="cljs-showcase">
+
+``` clojure
+(require '[medley.core :as medley])
+(medley/index-by :id [{:id 1} {:id 2}])
+```
+
+</div>
+
+<div style="width: 600px;" class="cljs-showcase">
+
+``` clojure
+(ns-publics 'medley.core)
 ```
 
 </div>
--- src/cljs_showcase/core.cljs
+++ src/cljs_showcase/core.cljs
@@ -6,21 +6,25 @@
    ["codemirror" :as cm]
    [promesa.core :as p]
    [sci.core :as sci]
-   [clojure.string :as str]))
+   [sci.ctx-store :as ctx-store]
+   [clojure.string :as str]
+   [medley.core]
+   [clojure.pprint :as pp]))
 
 (defn cm-string [cm-instance]
   (-> cm-instance .-state .-doc .toString))
 
-(def ctx (sci/init {:namespaces {}
-                    :classes {'js js/globalThis
-                              :allow :all
-                              'Math js/Math}
-                    :ns-aliases {'clojure.pprint 'cljs.pprint}}))
+(let [ctx (sci/init {:namespaces {}
+                     :classes {'js js/globalThis
+                               :allow :all
+                               'Math js/Math}
+                     :ns-aliases {'clojure.pprint 'cljs.pprint}})]
+  (ctx-store/reset-ctx! ctx))
 
 (defn eval-codemirror [cm-instance]
   (try
     (let [code-str (cm-string cm-instance)
-          v (sci/eval-string* ctx code-str)]
+          v (sci/eval-string* (ctx-store/get-ctx) code-str)]
       (if (instance? js/Promise v)
         (-> v
             (.then
@@ -35,60 +39,52 @@
 
 (defonce init-instances
   (let [elts (js/document.querySelectorAll ".cljs-showcase")]
-    (doseq [elt elts]
-      (let [doc (.-innerText elt)
+    (doseq [^js elt elts]
+      (let [_ (js/console.log (.. elt -dataset))
+            no-editable? (.. elt -dataset -cljsShowcaseNoEditable)
+            no-eval? (.. elt -dataset -cljsShowcaseNoEval)
+            eval? (not no-eval?)
+            doc (.-innerText elt)
             _ (set! (.-innerText elt) "")
             cm-ref (atom nil)
             res (js/document.createElement "pre")
             eval-me (fn []
-                      (p/let [[op v] (eval-codemirror @cm-ref)]
-                        (case op
-                          (:success :success-promise)
-                          (set! (.-innerText res)
-                                (str (when (= :success-promise op)
-                                       "Promise resolved to:\n")
-                                     (pr-str v)))
-                          (:error :error-promise)
-                          (set! (.-innerText res)
-                                (str/join "\n" (cons (.-message v)
-                                                     (sci/format-stacktrace (sci/stacktrace v))))))))
+                      (when eval?
+                        (p/let [[op v] (eval-codemirror @cm-ref)]
+                          (binding [*print-length* 20]
+                            (case op
+                              (:success :success-promise)
+                              (set! (.-innerText res)
+                                    (str (when (= :success-promise op)
+                                           "Promise resolved to:\n")
+                                         (with-out-str (pp/pprint v))))
+                              (:error :error-promise)
+                              (set! (.-innerText res)
+                                    (str/join "\n" (cons (.-message v)
+                                                         (sci/format-stacktrace (sci/stacktrace v))))))))))
             ext (.of cv/keymap
                      (clj->js [{:key "Mod-Enter"
-                                :run eval-me}
-                               #_{:key (str modifier "-Enter")
-                                  :shift (partial eval-top-level on-result)
-                                  :run (partial eval-at-cursor on-result)}]))
-            cm (cm/EditorView. #js {:doc doc
-                                    :extensions #js [cm/basicSetup, (lc/clojure), (.highest cs/Prec ext)]
-                                    :parent elt
-                                    #_#_:dispatch (fn [tr] (-> cm (.update #js [tr])) (eval-me))})]
-        (let [btn (js/document.createElement "button")
-              _ (set! (.-style btn) "float: right")
-              _ (set! btn -innerText "Eval")
-              _ (.addEventListener elt "click" eval-me)]
-          (.appendChild elt btn))
+                                :run eval-me}]))
+            state (cs/EditorState.create #js {:doc doc
+                                              :extensions #js [cm/basicSetup, (lc/clojure), (.highest cs/Prec ext),
+                                                               (cs/EditorState.readOnly.of no-editable?)]})
+            cm (cm/EditorView. #js {:state state :parent elt})]
+        (when eval?
+          (let [btn (js/document.createElement "button")
+                _ (set! (.-style btn) "float: right")
+                _ (set! btn -innerText "Eval")
+                _ (.addEventListener elt "click" eval-me)]
+            (.appendChild elt btn)))
         (.appendChild elt res)
         (reset! cm-ref cm)
-        (eval-me)))))
-
-(defn linux? []
-  (some? (re-find #"(Linux)|(X11)" js/navigator.userAgent)))
-
-(defn mac? []
-  (and (not (linux?))
-       (some? (re-find #"(Mac)|(iPhone)|(iPad)|(iPod)" js/navigator.platform))))
-
-;; (def default-eval-text "Eval")
-;; (let [elt (js/document.getElementById "evalMe")
-;;       txt default-eval-text
-;;       mod-symbol (if (mac?)
-;;                    "⌘"
-;;                    "⌃")
-;;       txt (str txt " " mod-symbol "-⏎")]
-;;   (set! (.-innerHTML elt) txt)
-;;   (.addEventListener elt "click" (fn []
-;;                                    (eval-cell))))
+        (when eval? (eval-me))))))
 
 (sci/alter-var-root sci/print-fn (constantly *print-fn*))
 (sci/alter-var-root sci/print-err-fn (constantly *print-err-fn*))
 (sci/enable-unrestricted-access!)
+
+;;;; User configuration begins here
+
+(def medley-ns (sci/copy-ns medley.core (sci/create-ns 'medley.core)))
+
+(ctx-store/swap-ctx! sci/merge-opts {:namespaces {'medley.core medley-ns}})
